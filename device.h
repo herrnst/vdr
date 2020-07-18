ee79a51925c1132609ee7e5d6cd02647f4d03a53
siricco <cco@aon.at>
2020-07-18 15:09:16 +0200

    vdr-2.4.4-camtweaks-2.4.1.patch

--- device.h
+++ device.h
@@ -86,6 +86,7 @@ struct tTrackId {
 class cPlayer;
 class cReceiver;
 class cLiveSubtitle;
+class cScaMapper;
 
 class cDeviceHook : public cListObject {
 public:
@@ -465,6 +466,10 @@ public:
 
 private:
   cCamSlot *camSlot;
+  cScaMapper *scaMapper;
+      /// Set if CamSlots static CAPMT mapping is active
+  bool scaMapMasterSlot;
+      /// True if static CAPMT mapping and MCD (but not MTD) is active
 public:
   virtual bool HasCi(void);
          ///< Returns true if this device has a Common Interface.
@@ -479,6 +484,8 @@ public:
   cCamSlot *CamSlot(void) const { return camSlot; }
          ///< Returns the CAM slot that is currently used with this device,
          ///< or NULL if no CAM slot is in use.
+  cScaMapper *ScaMapper(void) const { return scaMapper; }
+  bool ScaMapMasterSlot(void) const { return scaMapMasterSlot; }
 
 // Image Grab facilities
 
