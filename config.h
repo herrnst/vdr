7ea33c7b325a95474113a15da3959ae891475775
siricco <cco@aon.at>
2020-07-18 15:09:16 +0200

    vdr-2.4.4-camtweaks-2.4.1.patch

--- config.h
+++ config.h
@@ -366,6 +366,7 @@ public:
   int ChannelsWrap;
   int ShowChannelNamesWithSource;
   int EmergencyExit;
+  int EnableCamTweaks;
   int __EndData__;
   cString InitialChannel;
   cString DeviceBondings;
